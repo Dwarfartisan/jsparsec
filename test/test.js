3a83a0ea244eb85318d376264817280b83dbc631
liuliandroid <2458853753@qq.com>
2015-07-08 16:08:29 +0800

    test state

--- test/test.js
+++ test/test.js
@@ -1,5 +1,26 @@
 var jsParsec = require('../../jsparsec');
 
-var construtor = jsParsec.state;
+var op = jsParsec.getOperator;
 
-var state = new constructor();
+var states = ['A','B','C','D','E'];
+
+var state = new jsParsec.state(states);
+
+console.log(state.next());
+console.log(state.pos());
+
+
+console.log(state.next());
+console.log(state.pos())
+
+console.log(state.next());
+console.log(state.pos())
+
+var pred = function(item){
+    if (item === 'E') {
+        return true;
+    }else{
+        return false;
+    }
+};
+console.log(state.nextBy(pred));
