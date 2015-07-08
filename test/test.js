20923010ce9c5876f605ee41208fb12c6b725163
liuliandroid <2458853753@qq.com>
2015-07-08 22:32:42 +0800

    add UnitTest lib

--- test/test.js
+++ test/test.js
@@ -2,25 +2,54 @@ var jsParsec = require('../../jsparsec');
 
 var op = jsParsec.getOperator;
 
-var states = ['A','B','C','D','E'];
+var states = 'abcde';
 
 var state = new jsParsec.state(states);
 
-console.log(state.next());
-console.log(state.pos());
+var chai = require('chai');
+
+var assert = chai.assert;
+
+describe("state",function (){
+    describe("next",function () {
+        it("should return item in states'ss position",function (){
+            
+            assert.equal(state.next(),'a')
+        })
+    })
+    describe("pos",function () {
+        it("should return the pos",function() {
+            assert.equal(state.pos(),1);
+        })
+    })
+    describe("nextBy",function () {
+        it("should return b",function(){
+            assert.equal(state.nextBy(new Function("return false;")),'b')
+        })
+    })
+});
 
 
-console.log(state.next());
-console.log(state.pos())
 
-console.log(state.next());
-console.log(state.pos())
 
-var pred = function(item){
-    if (item === 'E') {
-        return true;
-    }else{
-        return false;
-    }
-};
-console.log(state.nextBy(pred));
+
+
+
+
+
+
+
+// console.log(state.next());
+// console.log(state.pos());
+
+
+// console.log(state.next());
+// console.log(state.pos())
+
+// console.log(state.next());
+// console.log(state.pos())
+
+// var pred = function(item){
+//     return item == 'D';
+// };
+// console.log(state.nextBy(pred));
